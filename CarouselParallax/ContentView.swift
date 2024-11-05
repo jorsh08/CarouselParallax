bb1ad6cf5d641f9fdffe7073888155ce21ccf50c
Jorge Verdugo <jorgeverdugo3005@gmail.com>
2024-11-05 08:52:43 -0700

    init project

--- CarouselParallax/ContentView.swift
+++ CarouselParallax/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    func getScale(proxy: GeometryProxy) -> CGFloat {
+        let midPoint: CGFloat = 190
+        let viewFrame = proxy.frame(in: CoordinateSpace.global)
+        var scale: CGFloat = 1.0
+        let deltaXAnimationThreshold: CGFloat = 125
+        let diffFromCenter = abs(midPoint - viewFrame.origin.x - deltaXAnimationThreshold / 2)
+        if diffFromCenter < deltaXAnimationThreshold {
+            scale = 1 + (deltaXAnimationThreshold - diffFromCenter) / 500
         }
-        .padding()
+        return scale
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 10) {
+                ForEach(0 ..< 10) { num in
+                    GeometryReader { proxy in
+                        let scale = getScale(proxy: proxy)
+                        VStack(spacing: 8) {
+                            Image(systemName: "person.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 120, height: 200)
+                                .background(Color.pink)
+                                .cornerRadius(20)
+                                .foregroundColor(.indigo)
+                        }
+                        .scaleEffect(.init(width: scale, height: scale))
+                        .animation(.spring(), value: 1)
+                    }
+                    .frame(width: 125, height: 250)
+                    .padding(.horizontal, 12)
+                    .padding(.leading, num == 0 ? 120 : 0)
+                    .padding(.trailing, num == 9 ? 90 : 0)
+                }
+                Spacer()
+                    .frame(width: 16)
+            }
+            .padding(.top, 30)
+        }
     }
 }
 
